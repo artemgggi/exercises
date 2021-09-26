a65816c551d4ddfefabd3df7facb07dee7cb9058
Artem <artem.ivanchikov.99@mail.ru>
2021-09-26 18:42:39 +0300

    первый коммит

--- src/main/java/ru/artemgggi/calculator/Calculator.java
+++ src/main/java/ru/artemgggi/calculator/Calculator.java
@@ -3,6 +3,7 @@ package ru.artemgggi.calculator;
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("1");
     }
 
 }
