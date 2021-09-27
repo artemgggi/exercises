bb4a22cd2c0c1a4c8d3e116f24a49a428de5f2c6
Artem <artem.ivanchikov.99@mail.ru>
2021-09-27 13:25:03 +0300

    тест

--- src/main/java/ru/artemgggi/calculator/Calculator.java
+++ src/main/java/ru/artemgggi/calculator/Calculator.java
@@ -1,9 +1,28 @@
 package ru.artemgggi.calculator;
 
 public class Calculator {
+
+    public static float rubleToEuro(float value) {
+        return value / 70;
+    }
+
+    public static double rubleToDollar(float value) {
+        return value / 65;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("1");
+        float in = 140;
+        float expectedEuro = 2;
+        float outEuro = Calculator.rubleToEuro(in);
+        boolean passedEuro = expectedEuro == outEuro;
+
+        double expectedDollar = 2.15;
+        double outDollar = Calculator.rubleToDollar(in);
+        boolean passedDollar = expectedDollar == outDollar;
+
+
+        System.out.println("140 rubles is Test result : " + passedEuro);
+        System.out.println("140 rubles is Test result : " + passedDollar);
     }
 
 }
