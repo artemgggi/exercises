0d3f0d1e46e5793f072d55d2d085538f0096aeb1
Artem <artem.ivanchikov.99@mail.ru>
2021-09-28 00:25:59 +0300

    weight test junit4

---
+++ src/test/java/ru/artemgggi/calculator/FitTest.java
@@ -0,0 +1,24 @@
+package ru.artemgggi.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FitTest {
+
+    @Test
+    public void whenMan180Then92() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman180Then() {
+        short in = 180;
+        double expected = 80.5;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
