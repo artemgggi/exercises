56a8b62d1148f89639945d94652ab0773d7fff8f
artemgggi <artem.ivanchikov.99@mail.ru>
2021-09-29 19:01:31 +0300

    fitness loop

---
+++ src/main/java/ru/artemgggi/loop/Fitness.java
@@ -0,0 +1,13 @@
+package ru.artemgggi.loop;
+
+public class Fitness {
+	public static int calc(int ivan, int nik) {
+		int month = 0;
+		while (ivan <= nik) {
+			month += 1;
+			ivan *= 3;
+			nik *= 2;
+		}
+		return month;
+	}
+}
