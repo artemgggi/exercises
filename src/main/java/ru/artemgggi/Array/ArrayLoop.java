e217ae2763f4b43a2a0dd2b924ff5e99cdd17c39
Artem <artem.ivanchikov.99@mail.ru>
2021-10-02 16:43:42 +0300

    debugging

--- src/main/java/ru/artemgggi/Array/ArrayLoop.java
+++ src/main/java/ru/artemgggi/Array/ArrayLoop.java
@@ -3,7 +3,7 @@ package ru.artemgggi.Array;
 public class ArrayLoop {
 	public static void main(String[] args) {
 		int[] array = new int[5];
-		for (int i = 0; i < array.length ; i++) {
+		for (int i = 0; i < array.length; i++) {
 			array[i] = i * 2 + 3;
 		}
 		for (int el : array) {
