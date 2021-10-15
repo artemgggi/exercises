d2628f4fee983082d21281900a812527223f8d99
artemgggi <artem.ivanchikov.99@mail.ru>
2021-10-15 13:10:55 +0300

    coffee machine change task

--- src/main/java/ru/artemgggi/array/Machine.java
+++ src/main/java/ru/artemgggi/array/Machine.java
@@ -8,8 +8,8 @@ public class Machine {
 		int[] coins = {10, 5, 2, 1};
 		int[] res = new int[100];
 		int size = 0;
-
 		int change = money - price;
+
 		for (int i = 0; i < coins.length; i++) {
 			while (change >= coins[i]) {
 				change = change - coins[i];
